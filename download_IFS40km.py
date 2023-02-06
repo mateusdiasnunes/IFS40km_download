4afdd0783414d3cfefc020cc5dde98852eff626f
Mateus Dias Nunes <81376987+mateusdiasnunes@users.noreply.github.com>
2023-02-06 09:41:17 -0300

    Update download_IFS40km.py

--- download_IFS40km.py
+++ download_IFS40km.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 # ============================================================================================================
-#     Script que realiza o download dos dados diários do ECMWF no formato grib2 transformando para netcdf.
-#                      Assim serão gerados os acumulados de chuva do IFS/ECMWF 40km
+#                  Script that downloads daily ECMWF data in grib2 format transforming to netcdf.
+#                      This will generate the accumulated rainfall of the IFS/ECMWF 40km
 #
-#                                Mateus Dias Nunes @WorldSE - Março de 2022
+#                                   Mateus Dias Nunes @WorldSE - March 2022
 #
 # ============================================================================================================
+
 
 from ecmwf.opendata import Client         # pip install ecmwf-api-client
 from datetime import datetime
@@ -83,7 +84,7 @@ filenameout = RECENT + 'ec_oper40_fc_' + today + '_f' +  str(init_hour).zfill(3)
 #('-------------------------------Information about https://github.com/ecmwf/ecmwf-opendata------------------------------------')
 #('----------------------------------------------------------------------------------------------------------------------------')
 
-# Downloading data type: forecast (fc), of strem: operacional (oper), to parameter: total precipitation (tp)
+# Downloading data type: forecast (fc), of strem: operational (oper), to parameter: total precipitation (tp)
 # In https://github.com/ecmwf/ecmwf-opendata has explanation about the variables 
 
 client = Client(source="ecmwf")
@@ -100,10 +101,10 @@ client.retrieve(
         filename)
 
 
-#Recortando a área da América do Sul
-cdo.mulc('1000', input='-sellonlatbox,-75,-30.2,-35,5 %s' % (filename), output=(filenameout)) # cdo mulc,1000 para passar os dados da unidade de kg.kg.m-2 para mm.dia-1
+#Cutting South America
+cdo.mulc('1000', input='-sellonlatbox,-75,-30.2,-35,5 %s' % (filename), output=(filenameout)) # cdo mulc,1000 to pass data file units from kg.kg.m-2 to mm.dia-1
 
-os.remove(filename) # remover arquivos não utilizados (otimizando o armazenamento)
+os.remove(filename) # remove data file 
 
 # Download loop
 for hour in range(init_hour, last_hour + 1, step_hour):
@@ -125,19 +126,20 @@ print('-------------------------------------------------------------------------
 
 #====================================================================================================================================
 
+
 # # # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# # #                                                    - Loop para calcular os acumulados de chuva de 12Z a 12Z
-# # #                                           OBS: os dados de de precipitação do forecast do IFS são acumulativos
-# # #                                                    Como são 41 tempos para cada horário de previsão (6h em 6h),
-# # #                                             será calculado sobre os tempo de previsão (41 tempos) e NÃO horários sinóticos
-# # #                                      Ex: 12Z a 12Z do primeiro dia é equivalente a subtração da CONTAGEM 06(36Z) menos a CONTAGEM 03(12Z)
-# # #                                            o segundo dia será da CONTAGEM 10 (60Z) menos a CONTAGEM (42Z) e assim sucessivamente.
+# # #                                                 - Loop to calculate cumulative precipitation from 12Z to 12Z
+# # #                                                       NOTE: IFS forecast precipitation data is cumulative
+# # #                                                     As there are 41 times for each forecast time (6h at 6h),
+# # #                                               will be calculated on forecast time (41 times) and NOT synoptic times
+# # #                                   Ex: 12Z to 12Z of the first day is equivalent to subtracting COUNT 06(36Z) minus COUNT 03(12Z)
+# # #                                               the second day will be COUNT 10 (60Z) minus COUNT (42Z) and so on.
 # # # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #################################################################################################################################################################################
-# DIAS       |    1     |       2     |       3     |      4     |       5        |      6          |        7        |        8        |         9       |         10      |
+# DAYS       |    1     |       2     |       3     |      4     |       5        |      6          |        7        |        8        |         9       |         10      |
 # HOUR_UTC   00 06 12 18| 00 06 12 18 | 00 06 12 18 |00 06 12 18 | 00  06  12  18 |  00  06  12  18 |  00  06  12  18 |  00  06  12  18 |  00  06  12  18 |  00  06  12  18 |  00 
-# CONTAGEM   00 01 02 03| 04 05 06 07 | 08 09 10 11 |12 13 14 15 | 16  17  18  19 |  20  21  22  23 |  24  25  26  27 |  28  29  30  31 |  32  33  34  35 |  36  37  38  39 |  40
+# COUNT      00 01 02 03| 04 05 06 07 | 08 09 10 11 |12 13 14 15 | 16  17  18  19 |  20  21  22  23 |  24  25  26  27 |  28  29  30  31 |  32  33  34  35 |  36  37  38  39 |  40
 # FORECAST(Z)00 06 12 18| 24 30 36 42 | 48 54 60 66 |72 78 84 90 | 96 102 108 114 | 120 126 132 138 | 144 150 156 162 | 168 174 180 186 | 192 198 204 210 | 216 222 228 234 | 240       
 #################################################################################################################################################################################
 
@@ -168,6 +170,7 @@ elif hour_run == '12':
        print(f'O valor de forecast: {forecast} ')
        cdo.sub(input  =  cdo.seltimestep( str(forecast), input  =  data_file) + ' ' + cdo.seltimestep( str(analysis), input  =  data_file), output  =  RECENT+'temp_ifs_oper_fc_'+today+'_'+str(forecast)+'_'+str(analysis)+'.grib2')  #python
 
+#CDO in python 
 #cdo.OPERATOR3("PARAM3",input="-OPERATOR2,PARAM2 -OPERATOR1,PARAM1 INPUT.grib2", output="OUTPUT.grib2")
 
 final_infile  = RECENT+'temp_ifs_oper_fc_'+today+'_*.grib2 ' #join data files 
@@ -178,10 +181,11 @@ final_outfile = RECENT+'ecmwf_SA_oper_fc_'+today+'_f'+str(init_hour).zfill(3)+'_
 # Change variable name from "tp" to "prec_acum_12Z" 
 # This was a standardization option so that all weather models have the same nomenclature on the precipitation variable between 12Z-12Z.
 # To pass format from grib2 to netcdf
+
 cdo.settaxis(str(dataformat)+',12:00:00,1day -chname,tp,prec_acum_12Z -mergetime', input=final_infile,  output=final_outfile, options='-f nc', returnCdf=True)
 
 os.system('mv ' +final_outfile+ ' ' + MODEL_path) #move data to final directory
-os.system('rm -R ' + RECENT ) #remove all temporary datafiles
+os.system('rm -R ' + RECENT )                     #remove all temporary datafiles
 
 #############################################################################################################################################################################################################################################################
 #############################################################################################################################################################################################################################################################
